00fa239facf624e51030b7e96dc5dd62f63b74a2
kencul <kenkoba02@icloud.com>
2026-02-27 03:13:50 +0900

    working messy version of webview ui

--- Source/inc/PluginEditor.h
+++ Source/inc/PluginEditor.h
@@ -1,29 +1,36 @@
 #pragma once
 
 #include "PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
-{
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor {
 public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
-    ~AudioPluginAudioProcessorEditor() override;
+	explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
+	~AudioPluginAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics &) override;
+	void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AudioPluginAudioProcessor& processorRef;
-    
-    // UI Components
-    juce::Slider gainSlider;
-    juce::Label gainLabel;
-    
-    // Attachment to connect slider to APVTS
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	AudioPluginAudioProcessor &processorRef;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+	juce::WebSliderRelay gainRelay;
+	juce::WebBrowserComponent webView;
+	juce::WebSliderParameterAttachment gainAttachment;
+
+	// UI Components
+	// juce::Slider gainSlider;
+	// juce::Label gainLabel;
+
+	// std::optional<juce::WebBrowserComponent::Resource> getResource(const juce::String &url);
+	// std::optional<juce::WebBrowserComponent::Resource>
+	// createResource(const char *data, size_t size, const juce::String &mime) const;
+	using Resource = juce::WebBrowserComponent::Resource;
+	std::optional<Resource> getResource(const juce::String &url) const;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
--- Source/src/PluginEditor.cpp
+++ Source/src/PluginEditor.cpp
@@ -1,22 +1,36 @@
 #include "PluginEditor.h"
 #include "PluginProcessor.h"
+#include <BinaryData.h>
 
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &p) :
-    AudioProcessorEditor(&p), processorRef(p) {
-	// Set up the gain slider
-	gainSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
-	gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20);
-	addAndMakeVisible(gainSlider);
+    AudioProcessorEditor(&p), processorRef(p), gainRelay {IDs::gain.getParamID()},
+    webView(juce::WebBrowserComponent::Options {}
+                .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
+                .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2 {}.withUserDataFolder(
+                    juce::File::getSpecialLocation(juce::File::SpecialLocationType::tempDirectory)))
+                .withNativeIntegrationEnabled()
+                .withResourceProvider([this](const juce::String &url) { return getResource(url); })
+                .withOptionsFrom(gainRelay)),
+    gainAttachment(*processorRef.apvts.getParameter(IDs::gain.getParamID()), gainRelay) {
+	// // Set up the gain slider
+	// gainSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+	// gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20);
+	// addAndMakeVisible(gainSlider);
 
-	// Set up the gain label
-	gainLabel.setText("Gain", juce::dontSendNotification);
-	gainLabel.setJustificationType(juce::Justification::centred);
-	addAndMakeVisible(gainLabel);
+	// // Set up the gain label
+	// gainLabel.setText("Gain", juce::dontSendNotification);
+	// gainLabel.setJustificationType(juce::Justification::centred);
+	// addAndMakeVisible(gainLabel);
 
-	// Attach the slider to the APVTS parameter
-	gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
-	    processorRef.apvts, IDs::gain.getParamID(), gainSlider);
+	// // Attach the slider to the APVTS parameter
+	// gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
+	//     processorRef.apvts, IDs::gain.getParamID(), gainSlider);
+
+	addAndMakeVisible(webView);
+
+	// Informs the browser to use the resource provider for the root page
+	webView.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
 
 	// Set the editor size
 	setSize(400, 300);
@@ -26,18 +40,68 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
 
 //==============================================================================
 void AudioPluginAudioProcessorEditor::paint(juce::Graphics &g) {
+	juce::ignoreUnused(g);
 	// Fill background
-	g.fillAll(juce::Colours::darkgrey);
+	// g.fillAll(juce::Colours::darkgrey);
 }
 
 void AudioPluginAudioProcessorEditor::resized() {
-	// Layout the gain control in the center of the window
-	auto bounds   = getLocalBounds();
-	auto knobArea = bounds.reduced(50);
+	webView.setBounds(getLocalBounds());
+	// // Layout the gain control in the center of the window
+	// auto bounds   = getLocalBounds();
+	// auto knobArea = bounds.reduced(50);
 
-	// Position the label at the top
-	gainLabel.setBounds(knobArea.removeFromTop(30));
+	// // Position the label at the top
+	// gainLabel.setBounds(knobArea.removeFromTop(30));
 
-	// Position the slider in the remaining space
-	gainSlider.setBounds(knobArea);
+	// // Position the slider in the remaining space
+	// gainSlider.setBounds(knobArea);
+}
+
+std::vector<std::byte> streamToVector(juce::InputStream &stream) {
+	using namespace juce;
+	const auto sizeInBytes = static_cast<size_t>(stream.getTotalLength());
+	std::vector<std::byte> result(sizeInBytes);
+	stream.setPosition(0);
+	[[maybe_unused]] const auto bytesRead = stream.read(result.data(), result.size());
+	jassert(bytesRead == static_cast<ssize_t>(sizeInBytes));
+	return result;
+}
+
+static const char *getMimeForExtension(const juce::String &extension) {
+	static const std::unordered_map<juce::String, const char *> mimeMap = {{{"htm"}, "text/html"},
+	                                                                       {{"html"}, "text/html"},
+	                                                                       {{"txt"}, "text/plain"},
+	                                                                       {{"jpg"}, "image/jpeg"},
+	                                                                       {{"jpeg"}, "image/jpeg"},
+	                                                                       {{"svg"}, "image/svg+xml"},
+	                                                                       {{"ico"}, "image/vnd.microsoft.icon"},
+	                                                                       {{"json"}, "application/json"},
+	                                                                       {{"png"}, "image/png"},
+	                                                                       {{"css"}, "text/css"},
+	                                                                       {{"map"}, "application/json"},
+	                                                                       {{"js"}, "text/javascript"},
+	                                                                       {{"woff2"}, "font/woff2"}};
+
+	if (const auto it = mimeMap.find(extension.toLowerCase()); it != mimeMap.end()) return it->second;
+
+	jassertfalse;
+	return "";
+}
+
+auto AudioPluginAudioProcessorEditor::getResource(const juce::String &url) const -> std::optional<Resource> {
+	std::cout << "ResourceProvider called with " << url << std::endl;
+
+	static const auto resourceFileRoot = juce::File {R"(C:\Users\Ken\Desktop\JUCE\simpleGain\Source\ui)"};
+
+	const auto resourceToRetrieve = url == "/" ? "index.html" : url.fromFirstOccurrenceOf("/", false, false);
+
+	const auto resource = resourceFileRoot.getChildFile(resourceToRetrieve).createInputStream();
+
+	if (resource) {
+		const auto extension = resourceToRetrieve.fromLastOccurrenceOf(".", false, false);
+		return Resource {streamToVector(*resource), getMimeForExtension(extension)};
+	}
+
+	return std::nullopt;
 }
